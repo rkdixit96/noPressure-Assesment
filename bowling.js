d04c7088b93342971e258b84f3f21b87b95e4cc0
krishna_dixit <rkdixit96@gmail.com>
2018-01-23 21:48:34 +0530

    Testing for simple case with no strikes or spare

--- bowling.js
+++ bowling.js
@@ -1,7 +1,27 @@
 function getScore(arr) {
-  const score = 0;
+  const strikeValue = 10;
+  const numberOfFrames = 10;
+
+  // Initializations
+  let score = 0;
+  const frameSums = [];
+  let currFrame = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      currFrame += 1;
+      frameSums[currFrame] = 0;
+    }
+    frameSums[currFrame] += arr[i];
+  }
+
+  // Sum all frame sums
+  for (let j = 0; j < numberOfFrames; j++) {
+    score += frameSums[j];
+  }
 
   return score;
 }
+
 
 module.exports = getScore;
--- bowling.test.js
+++ bowling.test.js
@@ -1,5 +1,13 @@
 const getScore = require('./bowling');
 
+describe('Testing with different input combinations', () => {
+  const testcase1 = [7, 1, 5, 3, 1, 4, 3, 5, 1, 8, 7, 2, 1, 6, 1, 1, 5, 1, 3, 1];
+
+  test('Input array has no strikes or spares', () => {
+    expect(getScore(testcase1)).toBe(66);
+  });
+});
+
 
 describe('Output validation', () => {
   const testcase = [10, 7, 2, 7, 3, 1, 1, 0, 0, 5, 4, 10, 1, 8, 5, 5, 2, 1];
