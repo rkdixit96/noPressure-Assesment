8e07115b10796d39a4cbfb32d67dafd946936168
krishna_dixit <rkdixit96@gmail.com>
2018-01-23 21:08:19 +0530

    Initial commit, setup of jest,eslint and testing of return type

---
+++ bowling.js
@@ -0,0 +1,7 @@
+function getScore(arr) {
+  const score = 0;
+
+  return score;
+}
+
+module.exports = getScore;
---
+++ bowling.test.js
@@ -0,0 +1,11 @@
+const getScore = require('./bowling');
+
+
+describe('Output validation', () => {
+  const testcase = [10, 7, 2, 7, 3, 1, 1, 0, 0, 5, 4, 10, 1, 8, 5, 5, 2, 1];
+
+  test('Returns a number', () => {
+    expect(typeof getScore(testcase)).toBe('number');
+  });
+});
+
